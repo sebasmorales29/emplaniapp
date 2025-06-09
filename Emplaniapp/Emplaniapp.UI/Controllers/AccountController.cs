286004ec9e1145b84c47c1d18202c33366b53303
HenryGonzalezAlguera <henry_cr999@hotmail.com>
2025-06-09 16:35:58 -0600

    fix account controller

--- Emplaniapp/Emplaniapp.UI/Controllers/AccountController.cs
+++ Emplaniapp/Emplaniapp.UI/Controllers/AccountController.cs
@@ -12,84 +12,69 @@ namespace Emplaniapp.UI.Controllers
     [Authorize]
     public class AccountController : Controller
     {
-        private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
+        private ApplicationSignInManager _signInManager;
 
         public AccountController() { }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        public AccountController(
+            ApplicationUserManager userManager,
+            ApplicationSignInManager signInManager)
         {
-            UserManager = userManager;
-            SignInManager = signInManager;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
-        public ApplicationUserManager UserManager
-        {
-            get => _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            private set => _userManager = value;
-        }
+        public ApplicationUserManager UserManager =>
+            _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
 
-        public ApplicationSignInManager SignInManager
-        {
-            get => _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
-            private set => _signInManager = value;
-        }
+        public ApplicationSignInManager SignInManager =>
+            _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
 
         private IAuthenticationManager AuthenticationManager =>
             HttpContext.GetOwinContext().Authentication;
 
-        // GET: /Account/Register
+        //
+        // GET: /Account/Login
         [AllowAnonymous]
-        public ActionResult Register()
+        public ActionResult Login(string returnUrl)
         {
-            var roleManager = HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
-            var model = new RegisterViewModel
-            {
-                RolesList = roleManager.Roles
-                    .Select(r => new SelectListItem { Value = r.Name, Text = r.Name })
-                    .ToList()
-            };
-            return View(model);
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
         }
 
-        // POST: /Account/Register
+        //
+        // POST: /Account/Login
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
         {
             if (!ModelState.IsValid)
-            {
-                // recargar RolesList antes de devolver la vista
-                var rm = HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
-                model.RolesList = rm.Roles
-                    .Select(r => new SelectListItem { Value = r.Name, Text = r.Name })
-                    .ToList();
                 return View(model);
-            }
 
-            var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
-            var result = await UserManager.CreateAsync(user, model.Password);
-            if (result.Succeeded)
+            var result = await SignInManager.PasswordSignInAsync(
+                model.UserName,
+                model.Password,
+                model.RememberMe,
+                shouldLockout: false
+            );
+
+            switch (result)
             {
-                // Asignar el rol elegido
-                await UserManager.AddToRoleAsync(user.Id, model.Role);
-
-                await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-                return RedirectToAction("Index", "Home");
+                case SignInStatus.Success:
+                    return RedirectToLocal(returnUrl);
+                case SignInStatus.LockedOut:
+                    return View("Lockout");
+                case SignInStatus.RequiresVerification:
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                default:
+                    ModelState.AddModelError("", "Usuario o contraseña incorrectos.");
+                    return View(model);
             }
-
-            AddErrors(result);
-
-            // recargar RolesList si hay error
-            var roleMgr = HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
-            model.RolesList = roleMgr.Roles
-                .Select(r => new SelectListItem { Value = r.Name, Text = r.Name })
-                .ToList();
-
-            return View(model);
         }
 
+        //
         // POST: /Account/LogOff
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -99,12 +84,82 @@ namespace Emplaniapp.UI.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        //
+        // GET: /Account/Register
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            var rm = HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            var model = new RegisterViewModel
+            {
+                RolesList = rm.Roles
+                    .Select(r => new SelectListItem { Value = r.Name, Text = r.Name })
+                    .ToList()
+            };
+            return View(model);
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                // recarga RolesList antes de devolver la vista
+                var rm = HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+                model.RolesList = rm.Roles
+                    .Select(r => new SelectListItem { Value = r.Name, Text = r.Name })
+                    .ToList();
+                return View(model);
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+            var result = await UserManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                // Asignar el rol elegido
+                await UserManager.AddToRoleAsync(user.Id, model.Role);
+
+                await SignInManager.SignInAsync(
+                    user, isPersistent: false, rememberBrowser: false
+                );
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Si falla, anexa errores y recarga RolesList
+            foreach (var error in result.Errors)
+                ModelState.AddModelError("", error);
+
+            var rm2 = HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            model.RolesList = rm2.Roles
+                .Select(r => new SelectListItem { Value = r.Name, Text = r.Name })
+                .ToList();
+
+            return View(model);
+        }
+
         #region Helpers
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+                return Redirect(returnUrl);
+            return RedirectToAction("Index", "Home");
+        }
+
         private void AddErrors(IdentityResult result)
         {
             foreach (var error in result.Errors)
                 ModelState.AddModelError("", error);
         }
+
         #endregion
     }
 }
