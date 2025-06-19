fd93e6484e4f5444e79bbd9bf105742f75c57fff
Yazmin <yrivera40037@ufide.ac.cr>
2025-06-19 02:07:39 -0600

    Se crean carpetas de datos personales y observacion

--- Emplaniapp/Emplaniapp.AccesoADatos/Hoja_Resumen/ListarHojaResumen/listarHojaResumenAD.cs
+++ Emplaniapp/Emplaniapp.AccesoADatos/Hoja_Resumen/ListarHojaResumen/listarHojaResumenAD.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Emplaniapp.Abstracciones.InterfacesAD.Hoja_Resumen.ListarHojaResumen;
-using Emplaniapp.Abstracciones.ModelosAD;
 using Emplaniapp.Abstracciones.ModelosParaUI;
 
 namespace Emplaniapp.AccesoADatos.Hoja_Resumen
