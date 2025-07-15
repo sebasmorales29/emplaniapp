d4f7c18fe6dcc729a6fe1e591155cc19c7883e3d
Yazmin <yrivera40037@ufide.ac.cr>
2025-07-14 20:06:57 -0600

    Se agrega el apartado de editar y agregar remuneracion, falta que lo haga de forma automatica

---
+++ Emplaniapp/Emplaniapp.Abstracciones/InterfacesAD/Remuneraciones/ObtenerRemuneracionPorId/IObtenerRemuneracionPorIdAD.cs
@@ -0,0 +1,9 @@
+﻿using Emplaniapp.Abstracciones.ModelosParaUI;
+
+namespace Emplaniapp.Abstracciones.InterfacesAD.Remuneraciones.ObtenerRemuneracionPorId
+{
+    public interface IObtenerRemuneracionPorIdAD
+    {
+        RemuneracionDto ObtenerPorId(int idRemuneracion);
+    }
+}
---
+++ Emplaniapp/Emplaniapp.LogicaDeNegocio/Remuneraciones/ObtenerRemuneracionPorId/ObtenerRemuneracionPorIdLN.cs
@@ -0,0 +1,22 @@
+﻿using Emplaniapp.Abstracciones.InterfacesAD.Remuneraciones.ObtenerRemuneracionPorId;
+using Emplaniapp.Abstracciones.InterfacesParaUI.Remuneraciones.ObtenerRemuneracionPorId;
+using Emplaniapp.Abstracciones.ModelosParaUI;
+using Emplaniapp.AccesoADatos.Remuneraciones.ObtenerRemuneracionPorId;
+
+namespace Emplaniapp.LogicaDeNegocio.Remuneraciones.ObtenerRemuneracionPorId
+{
+    public class ObtenerRemuneracionPorIdLN : IObtenerRemuneracionPorIdLN
+    {
+        private IObtenerRemuneracionPorIdAD _obtenerRemuneracionPorIdAD;
+
+        public ObtenerRemuneracionPorIdLN()
+        {
+            _obtenerRemuneracionPorIdAD = new ObtenerRemuneracionPorIdAD();
+        }
+
+        public RemuneracionDto ObtenerPorId(int idRemuneracion)
+        {
+            return _obtenerRemuneracionPorIdAD.ObtenerPorId(idRemuneracion);
+        }
+    }
+}
